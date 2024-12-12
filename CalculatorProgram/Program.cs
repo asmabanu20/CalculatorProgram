28127c61da47d4a81177ae602695da74ab2effe1
DELL <DELL@GAD>
2024-12-12 20:59:16 +0530

    Add project files.

---
+++ CalculatorProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProgram
+{
+    internal class calculator
+    {
+      
+       public int Add (int a,int b)
+        {
+            return a + b;
+        }
+        public double Add (double a,double b)
+        {
+            return a + b;
+
+        }
+        static void Main(string[] args)
+        {
+            calculator calculator1 = new calculator();          
+           int intsum= calculator1.Add(10,20);
+            Console.WriteLine("interger sum: "+intsum);
+
+           calculator calculator2 = new calculator();
+            double doublesum=calculator2.Add(20.57, 30.56);
+            Console.WriteLine("double sum: " + doublesum);
+        }
+    }
+}
